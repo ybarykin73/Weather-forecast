928b43a34bd4b947840422f9df8b8a57fb36d5ee
YuraBarykin <yBarykin@gmail.com>
2024-06-05 17:33:26 +0400

    Добавлено проксирование

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,6 +1,10 @@
 import React from "react"
-
+import Proxy from "../utils/proxy/Proxy"
 import { castCard } from "../factory/CardFactory"
+
+const proxy = new Proxy({
+  maxSize: 10
+})
 
 const useFetch  = (id) => {
   const [data, setData] = React.useState(null)
@@ -8,9 +12,17 @@ const useFetch  = (id) => {
 
   const getFetch = async () => {
     try {
+      if (proxy.has(id)) {
+        const card = proxy.get(id)
+        setData(card)
+        return
+      }
+
       const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?id=${id}&APPID=${process.env.API_KEY}`)      
       const data = await response.json()
       const card = castCard(data)
+
+      proxy.set(id, card)
       setData(card)
     } catch (error) {
       console.error(error)
---
+++ src/utils/proxy/Proxy.js
@@ -0,0 +1,30 @@
+export default class Proxy {
+  proxyCache = new Map()
+
+  constructor ({
+    maxSize = 10
+  } = {}) {
+    this.maxSize = maxSize
+  }
+
+  set (key = '', value) {
+    if (this.size() === this.maxSize) {
+      const oldestKey = [...this.proxyCache.keys()].shift()
+      this.proxyCache.delete(oldestKey)
+    }
+
+    this.proxyCache.set(key, value)
+  }
+
+  has (key) {
+    return this.proxyCache.has(key)
+  }
+
+  get (key) {
+    return this.proxyCache.get(key)
+  }
+
+  size () {
+    return this.proxyCache.size
+  }
+}
